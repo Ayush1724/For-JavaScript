778a74c5fdcd38b42713088ca37fac211370be02
Ayush1724 <ayushshrivastava2015@gmail.com>
2024-12-27 16:37:52 +0530

    Success

---
+++ button1.js
@@ -0,0 +1,8 @@
+let bt1= document.querySelector('#bt1')
+bt1.addEventListener('click',onclick)
+function onclick(){
+    let head=document.querySelector('#head')
+    head.innerHTML="Cybrom"
+    bt1.textContent="Remove"
+}
+    
