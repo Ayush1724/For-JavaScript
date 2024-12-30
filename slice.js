f73950fc36e37da19b75186d99083c54f0871c0f
Ayush1724 <ayushshrivastava2015@gmail.com>
2024-12-30 19:49:51 +0530

    Success

---
+++ slice.js
@@ -0,0 +1,9 @@
+let ar = [1,2,3,4,5,6,7,8,9]
+console.log(ar);
+
+let del = ar.slice(1,5)
+console.log(del);
+
+console.log(ar);
+
+
---
+++ splice.js
@@ -0,0 +1,8 @@
+let ar = [1,2,3,4,5,6,7,8,9]
+console.log(ar);
+
+// ar.splice(1,4)
+// console.log(ar);
+
+ar.splice(1,4,"Code","Solve")
+console.log(ar);
