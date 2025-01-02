6bf051b7d54ca0137ebfb057ebc8d8ce8376c472
Ayush1724 <ayushshrivastava2015@gmail.com>
2025-01-02 19:43:02 +0530

    Success

---
+++ objectfunction.js
@@ -0,0 +1,9 @@
+let ob1 = {
+    name : "Ayush",
+    demo : function na(){
+        alert("inside object")
+    },
+    friends : ["Shivam","Uday","Anuj","Chin2"]
+}
+ob1.demo()
+console.log(ob1.friends[1]);
