6a466ccd57282c87b46017c141aa909b5627ce35
Ayush1724 <ayushshrivastava2015@gmail.com>
2025-01-02 19:32:33 +0530

    Success

---
+++ nestedobject.js
@@ -0,0 +1,14 @@
+let emp = {
+    emp_name : "Ayush",
+    emp_id : 101,
+    emp_age : 24,
+    emp_designation :"FSD",
+    emp_account :{
+        emp_salary : 15000,
+        emp_acname :"SBI",
+        emp_acbalace : "RS 5000/- only*"
+    }
+}
+// console.log(emp);
+
+console.log(emp.emp_account.emp_acbalace);
