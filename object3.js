96afd73ea5c9ece51224ce28248cd706a89c6d6f
Ayush1724 <ayushshrivastava2015@gmail.com>
2025-01-03 19:22:18 +0530

    Success

---
+++ object3.js
@@ -0,0 +1,15 @@
+let ob = {
+    name : "Ayush",
+    age : 24,
+    Location : "Cybrom",
+    STack : "FSD"
+}
+let val = Object.values(ob)
+console.log(val);
+let k = Object.keys(ob)
+console.log(k);
+
+delete ob.age
+console.log(ob);
+
+
