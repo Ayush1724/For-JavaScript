db70948a2e2774892dee312403993e0054b2dd16
Ayush1724 <ayushshrivastava2015@gmail.com>
2025-01-04 19:23:50 +0530

    Success

---
+++ ob1.js
@@ -0,0 +1,18 @@
+let a = [
+    {
+        name:"Ayush",
+        age:24,
+        contact:12345
+    },
+    {
+        name:"Anuj",
+        age:24,
+        contact:56789
+    },
+    {
+        name:"Ram",
+        age:45,
+        Contact:221133
+    }
+]
+console.log(a);
