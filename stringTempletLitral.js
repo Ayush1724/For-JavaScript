45496ba3267409c36ff5e986a6e76d3e15c4b448
Ayush1724 <ayushshrivastava2015@gmail.com>
2025-01-07 19:39:48 +0530

    Success

--- stringTempletLitral.js
+++ stringTempletLitral.js
@@ -5,3 +5,14 @@ how are you
 I am FDS ${tr}`
 
 console.log(str1);
+
+
+let fname ="Ayush"
+let lname = "Shrivastava"
+
+console.log(fname+lname);
+
+let name = fname.concat(" ",lname)
+console.log(name);
+
+
