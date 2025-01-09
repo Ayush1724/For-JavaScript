c297d63b06c6117e136d7a323c4242a6ce3a3e22
Ayush1724 <ayushshrivastava2015@gmail.com>
2025-01-09 19:52:21 +0530

    Success

--- task.js
+++ task.js
@@ -4,50 +4,60 @@ let head =document.querySelector('#head')
 function tgl1(){
     let a = document.querySelector('#s1')
     a.classList.toggle('abc')
-    head.innerHTML=+380
+    let b = parseInt(prompt("Enter the number"))
+    head.innerHTML=380*b
 }
 function tgl2(){
     let a = document.querySelector('#s2')
     a.classList.toggle('abc')
-    head.innerHTML=+250
+    let b = parseInt(prompt("Enter the number"))
+    head.innerHTML=250*b
 }
 function tgl3(){
     let a = document.querySelector('#s3')
     a.classList.toggle('abc')
-    head.innerHTML=+250
+    let b = parseInt(prompt("Enter the number"))
+    head.innerHTML=250*b
 }
 function tgl4(){
     let a = document.querySelector('#s4')
     a.classList.toggle('abc')
-    head.innerHTML=+250
+    let b = parseInt(prompt("Enter the number"))
+    head.innerHTML=250*b
 }
 function tgl5(){
     let a = document.querySelector('#s5')
     a.classList.toggle('abc')
-    head.innerHTML=+380
+    let b = parseInt(prompt("Enter the number"))
+    head.innerHTML=380*b
 }
 function tgl6(){
     let a = document.querySelector('#a1')
     a.classList.toggle('abc')
-    head.innerHTML=+380
+    let b = parseInt(prompt("Enter the number"))
+    head.innerHTML=380*b
 }
 function tgl7(){
     let a = document.querySelector('#a2')
     a.classList.toggle('abc')
-    head.innerHTML=+250
+    let b = parseInt(prompt("Enter the number"))
+    head.innerHTML=250*b
 }
 function tgl8(){
     let a = document.querySelector('#a3')
     a.classList.toggle('abc')
-    head.innerHTML=+250
+    let b = parseInt(prompt("Enter the number"))
+    head.innerHTML=250*b
 }
 function tgl9(){
     let a = document.querySelector('#a4')
     a.classList.toggle('abc')
-    head.innerHTML=+250
+    let b = parseInt(prompt("Enter the number"))
+    head.innerHTML=250*b
 }
 function tgl10(){
     let a = document.querySelector('#a5')
     a.classList.toggle('abc')
-    head.innerHTML=+380
+    let b = parseInt(prompt("Enter the number"))
+    head.innerHTML=380*b
 }
