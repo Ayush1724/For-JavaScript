093b3364fec18736361b703139dce22c76b92b33
Ayush1724 <ayushshrivastava2015@gmail.com>
2025-01-09 14:06:22 +0530

    Success

---
+++ task.js
@@ -0,0 +1,53 @@
+let head =document.querySelector('#head')
+// let a=380
+// let b=250
+function tgl1(){
+    let a = document.querySelector('#s1')
+    a.classList.toggle('abc')
+    head.innerHTML=+380
+}
+function tgl2(){
+    let a = document.querySelector('#s2')
+    a.classList.toggle('abc')
+    head.innerHTML=+250
+}
+function tgl3(){
+    let a = document.querySelector('#s3')
+    a.classList.toggle('abc')
+    head.innerHTML=+250
+}
+function tgl4(){
+    let a = document.querySelector('#s4')
+    a.classList.toggle('abc')
+    head.innerHTML=+250
+}
+function tgl5(){
+    let a = document.querySelector('#s5')
+    a.classList.toggle('abc')
+    head.innerHTML=+380
+}
+function tgl6(){
+    let a = document.querySelector('#a1')
+    a.classList.toggle('abc')
+    head.innerHTML=+380
+}
+function tgl7(){
+    let a = document.querySelector('#a2')
+    a.classList.toggle('abc')
+    head.innerHTML=+250
+}
+function tgl8(){
+    let a = document.querySelector('#a3')
+    a.classList.toggle('abc')
+    head.innerHTML=+250
+}
+function tgl9(){
+    let a = document.querySelector('#a4')
+    a.classList.toggle('abc')
+    head.innerHTML=+250
+}
+function tgl10(){
+    let a = document.querySelector('#a5')
+    a.classList.toggle('abc')
+    head.innerHTML=+380
+}
