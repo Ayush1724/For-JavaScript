fe274bc363f2d1b670d13c13cb043b904eeeb722
Ayush1724 <ayushshrivastava2015@gmail.com>
2025-01-13 19:34:40 +0530

    Success

---
+++ array2.js
@@ -0,0 +1,14 @@
+let arr=[]
+let b=10
+function fun(){
+    let a =document.querySelector('#ip').value
+    let head=document.querySelector('#head')
+    for(let b=0;b<10;b++){
+        arr[b]=a
+    }
+    head.innerHTML=a
+    for(let v of arr){
+        
+    }
+    
+}
