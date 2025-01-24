5afd1dea6ccce05a3bf62ccade1b93e4f2a57c62
Ayush1724 <ayushshrivastava2015@gmail.com>
2025-01-24 19:29:29 +0530

    Success

---
+++ localstorage1.js
@@ -0,0 +1,7 @@
+let person_ob = {
+    name : "Ayush Shrivastava",
+    contact : 8770644508,
+    email : "ayush@gmail.com",
+    password : "12345vh"
+}
+localStorage.setItem("userdata",JSON.stringify(person_ob))
