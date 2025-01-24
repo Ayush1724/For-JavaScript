bc4e711ff563b4ddcb8a08abba5ea6cc8e39408f
Ayush1724 <ayushshrivastava2015@gmail.com>
2025-01-24 16:02:01 +0530

    Success

--- localstorage.js
+++ localstorage.js
@@ -1,3 +1,9 @@
 localStorage.setItem("Name","Ayush Shrivastava")
 let a = 8770644508
 localStorage.setItem("Contact",a)
+localStorage.setItem("Email","AyushShrivastava@123Gmail.com")
+localStorage.setItem("Address","Ashoka garden")
+let b=localStorage.getItem("Contact")
+document.write(b)
+localStorage.removeItem("Email")
+localStorage.clear()
