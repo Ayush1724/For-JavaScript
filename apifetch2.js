7b0c8272d1bb57fe42724b3f3de42d3b445de5b1
Ayush1724 <ayushshrivastava2015@gmail.com>
2025-02-06 19:47:05 +0530

    Success

--- apifetch2.js
+++ apifetch2.js
@@ -21,3 +21,20 @@ function mydelete(id){
     .then(res=>alert("Deleted"))
     
 }
+
+function savedata(){
+    let frmdata = {
+        name:document.querySelector("#name").value,
+        age:document.querySelector("#age").value,
+        contact:document.querySelector("#contact").value,
+        city:document.querySelector("#city").value
+    }
+    fetch('http://localhost:3000/student',{
+        method:"POST",
+        headers:{
+            'Content-type':'application/json'
+        },
+        body:JSON.stringify(frmdata)
+    })
+    .then(r=>alert("Data inserted successfully...!!!"))
+}
